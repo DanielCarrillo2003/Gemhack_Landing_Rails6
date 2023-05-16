f704789033cde264da665b2d5cecf0f542a5c8dc
Daniel Carrillo <daniel_carrillo_2003@outlook.com>
2023-05-15 19:10:12 -0600

    Scroll effect, navbar, footer

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,38 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
+
+import "bootstrap";
+
+const menu = document.getElementById("menu-icon");
+const navegacion = document.querySelector(".navegacion");
+
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navegacion.classList.toggle("open");
+};
+
+window.addEventListener("scroll", reveal);
+function reveal() {
+  const reveals = document.querySelectorAll(".reveal");
+
+  for (let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let revealTop = reveals[i].getBoundingClientRect().top;
+    let revealPoint = 100;
+
+    if (revealTop < windowHeight - revealPoint) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
