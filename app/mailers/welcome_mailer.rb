3dd66e11a5a3e03b581b2a2fe552a5c7ebd09534
Daniel Carrillo <daniel_carrillo_2003@outlook.com>
2023-05-20 17:22:53 -0600

    Update some aspects of mailer

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -1,6 +1,6 @@
 class WelcomeMailer < ApplicationMailer
   def notify(correo)
     @correo = correo
-    mail to: correo.email, subject: correo.message
+    mail to: "daniel_carrillo_2003@gmail.com", subject: correo.message
   end
 end
