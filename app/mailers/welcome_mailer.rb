73a06182ae76c0933b53ad2f236f886c2aa9e150
Daniel Carrillo <daniel_carrillo_2003@outlook.com>
2023-05-20 19:44:17 -0600

    Direcciones de correo actualizadas

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -1,6 +1,6 @@
 class WelcomeMailer < ApplicationMailer
   def notify(correo)
     @correo = correo
-    mail to: "saratiel69@gmail.com", subject: correo.message
+    mail to: "javierjorge77@hotmail.com, admin@gemhack.io, saratiel69@gmail,com", subject: correo.message
   end
 end
